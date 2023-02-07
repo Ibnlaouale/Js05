763e1921ec5addc3b4c4b873df49f385eedb10a4
dinilaouale@gmail.com <dinilaouale@gmail.com>
2023-02-07 07:38:04 +0100

    commit1

---
+++ Js00/product.js
@@ -0,0 +1,6 @@
+var x=250;
+var y=250;
+var z=250;
+var somme;
+somme=x+y+z;
+console.log(somme);
---
+++ Js00/swapValues.js
@@ -0,0 +1,7 @@
+var a=10;
+var b=20;
+var tmp;
+tmp=a;
+a=b;
+b=tmp;
+console.log("New value of a: ",a, ' \n',"New value of b: ",b,'\n');
