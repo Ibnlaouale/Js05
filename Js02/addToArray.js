5c4020d7397605dcd5c9913e9c9e28aebcd01ee1
dinilaouale@gmail.com <dinilaouale@gmail.com>
2023-02-10 18:43:49 +0100

    3ème essai

--- Js02/addToArray.js
+++ Js02/addToArray.js
@@ -1,5 +1,5 @@
 const myArray = [["John", 23], ["cat", 2]];
 
-const mat = myArray.push(["C3", 1]);
+myArray.push(["Cs", 1]);
 
 console.log(myArray);
--- Js02/myStr.js
+++ Js02/myStr.js
@@ -1,5 +1,1 @@
-let chaine1 = "This is the start";
-
-let chaine2 = "This is the end";
-
-console.log(chaine1+" "+chaine2);
+console.log("This is the start "+" This is the end");
