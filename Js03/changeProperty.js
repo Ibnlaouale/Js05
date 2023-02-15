fefd45545ba4d782d0d6aa1e1b127cb7d3e5ec36
dinilaouale@gmail.com <dinilaouale@gmail.com>
2023-02-15 14:54:03 +0100

    deuxième essai

--- Js03/changeProperty.js
+++ Js03/changeProperty.js
@@ -2,14 +2,9 @@ const codeloccol = {
   name: 'Coder',
   legs: 4,
   tails: 1,
-  friends: '[Codeloccol Campers]',
+  friends: ['Codeloccol Campers'],
 };
 
-codeloccol.name = "Happy Coder";
+codeloccol.name = 'Happy Coder';
 
-console.log (
-  "name: ",codeloccol.name,'\n',
-  "legs: ", codeloccol.legs,'\n',
-  "tails: ",codeloccol.tails, '\n', 
-  "freiends: ",codeloccol.friends
-  );
+console.log (codeloccol);
